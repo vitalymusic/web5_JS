c80a7ec6d67174f5908467695a28b36bd56220d7
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-01-26 21:03:10 +0200

    JS teksta un skaitlu funkcijas

--- mainigie.js
+++ mainigie.js
@@ -11,7 +11,7 @@ skaitlis1 = 2026; //Number
 
 let bool = true; //Boolean
 
-let str1 = "<h1>Tas ir teksts</h1>";
+let str1 = "Tas ir teksts";
 
 console.log(skaitlis1);
 
@@ -58,13 +58,17 @@ document.body.innerHTML = Math.trunc(generate1);
 
 
 
-
+let background = {
+    red:"red",
+    green:"green",
+    blue:"blue"
+};
 
 setInterval(()=>{
 
 
     // RGB
-    let background = {
+    background = {
     red: (Math.random()*255).toFixed(),
     green: (Math.random()*255).toFixed(),
     blue: (Math.random()*255).toFixed(),
@@ -75,6 +79,39 @@ setInterval(()=>{
     
 },1000)
 
+
+// Darbības ar Tekstu
+
+let str1_1 = "<h1 style=\"color:red\">Tas ir teksts</h1>";
+let str2 = '<h1 style="color:'+background.red+'">'; //konkatenācija
+    str2 += 'Tas ir teksts';
+    str2 += '</h1>';
+let str3  = `
+    <h1 style="color:${background.green}">
+        ${str1}
+    </h1>    
+`;
+
+// document.body.innerHTML+=str2;
+document.body.innerHTML+=str3;
+
+arr3 = ["Volvo","Mercedes"]
+// Teksta funkcijas
+
+console.log(str2[4]);
+console.log(str2.length);
+console.log(str2.split("\""));
+console.log(arr3.toString());
+
+console.log(str2.slice(3,20));
+console.log(str2.toLowerCase());
+console.log(str2.toUpperCase());
+console.log(String("   ok   ").trim());
+
+console.log(str3.search("</h1>"));
+console.log(str3.search("<//h1>"));
+
+document.body.innerHTML = str3.repeat(10);
 
 
 
