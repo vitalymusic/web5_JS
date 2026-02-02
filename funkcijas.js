673be3517c36c2686a95062cf2be89ac36977dc2
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-02-02 18:38:21 +0200

    funkcijas

--- funkcijas.js
+++ funkcijas.js
@@ -1,15 +1,27 @@
 function sayHello(text,type){
+    text = text.toUpperCase();
+
     switch(type){
         case "alert":alert(text);break;
         case "prompt":prompt(text);break;
         case "confirm":confirm(text);break;
     }
 
+    return 2+2;
+
+    
+
 }
 
 
 const clickHandler = ()=>{
-        sayHello("Hello","confirm");
+      document.body.innerHTML+= `<h1>Funkcijas rezultāts ir: ${sayHello("Hello","confirm")} </h1>`
+}
+
+
+const counter = ()=>{
+        
+
 }
 
 
